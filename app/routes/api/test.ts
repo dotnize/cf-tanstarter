f91a32e180906ef92e141bba34e01192af4e75f8
Nathaniel Tampus <nate@mugnavo.com>
2025-03-09 22:16:27 +0800

    new api route

---
+++ app/routes/api/test.ts
@@ -0,0 +1,8 @@
+import { json } from '@tanstack/react-start'
+import { createAPIFileRoute } from '@tanstack/react-start/api'
+
+export const APIRoute = createAPIFileRoute('/api/test')({
+  GET: () => {
+    return json({ message: 'Hello "/api/test"!' })
+  },
+})
